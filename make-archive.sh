4e5e4b80b8137a348d019f72203937aca766342b
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-29 01:21:56 +0900

    fix: アーカイブファイル名に日付付与

--- make-archive.sh
+++ make-archive.sh
@@ -2,9 +2,8 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 BASE_NAME=$(basename $SCRIPT_DIR)
-ARCHIVE=$BASE_NAME.tar.gz
 
-tar czf $ARCHIVE -C $BASE_DIR \
-    --exclude=$BASE_NAME/$ARCHIVE \
+tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+    --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
     --exclude=$BASE_NAME/vendor/bundle \
     $BASE_NAME
