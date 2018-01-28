27403ef2241d8e3473bf3e125abb454a96b4f83d
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-29 00:49:37 +0900

    add: ダウンロード済みのソースを含めてアーカイブするスクリプトを追加

---
+++ make-archive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+BASE_DIR=$(dirname $SCRIPT_DIR)
+BASE_NAME=$(basename $SCRIPT_DIR)
+ARCHIVE=$BASE_NAME.tar.gz
+
+tar czf $ARCHIVE -C $BASE_DIR \
+    --exclude=$BASE_NAME/$ARCHIVE \
+    --exclude=$BASE_NAME/vendor/bundle \
+    $BASE_NAME
